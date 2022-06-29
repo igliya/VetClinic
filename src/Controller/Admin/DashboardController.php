66f243bde67c7c1b9eac9b82fc2b19e30a7fd01a
igliya <l4195662@gmail.com>
2022-06-29 19:18:36 +0300

    feat(app): fix

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -32,24 +32,43 @@ class DashboardController extends AbstractDashboardController
 
     public function configureMenuItems(): iterable
     {
+        yield MenuItem::linkToDashboard('Главная', 'fa fa-home');
         yield MenuItem::section();
-        yield MenuItem::linkToCrud('Услуги', 'fas fa-list', Service::class);
-        yield MenuItem::linkToCrud('Типы животных', 'fas fa-cat', AnimalKind::class);
-        yield MenuItem::linkToCrud('Сотрудники', 'fas fa-user-circle', User::class);
-        yield MenuItem::linkToUrl(
-            'Отчёт по услугам',
-            'fas fa-file-pdf',
-            $this->generateUrl('report_services', [], true)
-        );
-        yield MenuItem::linkToUrl(
-            'Отчёт по докторам',
-            'fas fa-file-pdf',
-            $this->generateUrl('report_doctors', [], true)
-        );
-        yield MenuItem::linkToUrl(
-            'Статистика',
-            'fa fa-bar-chart',
-            "http://practice.igliya.ru"
-        );
+
+        yield MenuItem::subMenu('Пациенты');
+        yield MenuItem::linkToCrud('Клиенты', 'fas fa-user-circle', User::class);
+        yield MenuItem::linkToCrud('Животные', 'fas fa-cat', User::class);
+
+        yield MenuItem::subMenu('Справочники');
+        yield MenuItem::linkToCrud('Породы', 'fa fa-bars', User::class);
+        yield MenuItem::linkToCrud('Виды', 'fa fa-list', User::class);
+        yield MenuItem::linkToCrud('Услуги', 'fa fa-list-ol', Service::class);
+
+        yield MenuItem::subMenu('Организация');
+        yield MenuItem::linkToCrud('Сотрудники', 'fa fa-user', User::class);
+        yield MenuItem::linkToCrud('Расписание', 'fa fa-calendar', User::class);
+        yield MenuItem::linkToCrud('Отчёты', 'fas fa-file-pdf', User::class);
+        yield MenuItem::linkToCrud('Статистика', 'fa fa-bar-chart', User::class);
+
+
+//        yield MenuItem::section();
+//        yield MenuItem::linkToCrud('Услуги', 'fas fa-list', Service::class);
+//        yield MenuItem::linkToCrud('Типы животных', 'fas fa-cat', AnimalKind::class);
+//        yield MenuItem::linkToCrud('Сотрудники', 'fas fa-user-circle', User::class);
+//        yield MenuItem::linkToUrl(
+//            'Отчёт по услугам',
+//            'fas fa-file-pdf',
+//            $this->generateUrl('report_services', [], true)
+//        );
+//        yield MenuItem::linkToUrl(
+//            'Отчёт по докторам',
+//            'fas fa-file-pdf',
+//            $this->generateUrl('report_doctors', [], true)
+//        );
+//        yield MenuItem::linkToUrl(
+//            'Статистика',
+//            'fa fa-bar-chart',
+//            "http://practice.igliya.ru"
+//        );
     }
 }
